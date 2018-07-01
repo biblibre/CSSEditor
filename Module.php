bb22c193879eaf7b3f29ab3632ae7ac463883196
Daniel Berthereau <Daniel.github@Berthereau.net>
2018-07-02 00:00:00 +0200

    Cleaned Module.php.

--- Module.php
+++ Module.php
@@ -1,14 +1,13 @@
 <?php
 namespace CSSEditor;
 
-use HTMLPurifier;
-use HTMLPurifier_Config;
+use Omeka\Module\AbstractModule;
 use Zend\Mvc\Controller\AbstractController;
 use Zend\View\Renderer\PhpRenderer;
 use Zend\Form\Element\Textarea;
 use Zend\EventManager\SharedEventManagerInterface;
 use Zend\EventManager\Event;
-use Omeka\Module\AbstractModule;
+use Zend\ModuleManager\ModuleManager;
 
 /**
  * CSS Editor
@@ -24,12 +23,27 @@ use Omeka\Module\AbstractModule;
 
 class Module extends AbstractModule
 {
-    public function init()
+    public function init(ModuleManager $moduleManager)
     {
         require_once __DIR__ . '/vendor/autoload.php';
     }
 
-    public function getConfigForm(PhpRenderer $renderer) {
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function attachListeners(SharedEventManagerInterface $sharedEventManager)
+    {
+        $sharedEventManager->attach(
+            '*',
+            'view.layout',
+            [$this, 'appendCss']
+        );
+    }
+
+    public function getConfigForm(PhpRenderer $renderer)
+    {
         $serviceLocator = $this->getServiceLocator();
         $settings = $serviceLocator->get('Omeka\Settings');
 
@@ -43,6 +57,7 @@ class Module extends AbstractModule
 
     public function handleConfigForm(AbstractController $controller)
     {
+        /** @var \CSSEditor\Service\CssCleaner $cssCleaner */
         $cssCleaner = $this->getServiceLocator()->get('CSSEditor\CssCleaner');
 
         $css = $controller->getRequest()->getPost('css', '');
@@ -58,12 +73,14 @@ class Module extends AbstractModule
         return true;
     }
 
-    public function setOption($name, $value) {
+    protected function setOption($name, $value)
+    {
         $serviceLocator = $this->getServiceLocator();
         return $serviceLocator->get('Omeka\Settings')->set($name,$value);
     }
 
-    protected function setSiteOption($site_id, $name, $value) {
+    protected function setSiteOption($site_id, $name, $value)
+    {
         $serviceLocator = $this->getServiceLocator();
         $siteSettings = $serviceLocator->get('Omeka\Settings\Site');
         $entityManager = $serviceLocator->get('Omeka\EntityManager');
@@ -76,7 +93,13 @@ class Module extends AbstractModule
         return false;
     }
 
-    public function appendCss(Event $event) {
+    /**
+     * Append css to the current site.
+     *
+     * @param Event $event
+     */
+    public function appendCss(Event $event)
+    {
         $serviceLocator = $this->getServiceLocator();
         $siteSettings = $serviceLocator->get('Omeka\Settings\Site');
         $settings = $serviceLocator->get('Omeka\Settings');
@@ -89,13 +112,5 @@ class Module extends AbstractModule
         } elseif ($css = $settings->get('css_editor_css')) {
             $view->headStyle()->appendStyle($css);
         }
-    }
-
-    public function attachListeners(SharedEventManagerInterface $sharedEventManager) {
-        $sharedEventManager->attach('*', 'view.layout', [$this, 'appendCss']);
-    }
-
-    public function getConfig() {
-        return include __DIR__ . '/config/module.config.php';
     }
 }
